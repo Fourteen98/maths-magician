1f7f338bc95577f125638c3e1acfbbbc8074f217
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-12 23:54:26 +0000

    added new pages

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+function Quote() {
+  return (
+    <div className="quote">
+      <p>
+
+        Mathmatics is not about numbers,eauations, computations,
+        or algorithms: it is about understanding.
+        <span className="author"><i>-William Paul Thurston</i></span>
+      </p>
+    </div>
+  );
+}
+
+export default Quote;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './styles/styles.css';
+import {
+  BrowserRouter as Router, Routes,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/Nav';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/app" element={<App />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </Router>,
+  document.getElementById('root'),
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
