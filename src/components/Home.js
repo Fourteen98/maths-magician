e499b3fcb7801c98f855905d842829299dcea257
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-13 00:00:54 +0000

    add details to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ function Home() {
       <h2>Welcome to our page!</h2>
       <p>
         You can have your maths calculations here.
+        Go to the Calculator section from the Navbar to get started.
       </p>
     </div>
   );
